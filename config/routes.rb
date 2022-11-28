e47cff0df7e7582a44084998e654df08531f7f55
Agustin Morere <amorere10@gmail.com>
2022-11-28 16:42:27 -0300

    rutas, controllers  y acciones sin contenido

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  get 'booking/new'
-  get 'booking/update'
-  get 'booking/create'
-  get 'booking/pay'
-  get 'cars/index'
-  get 'cars/show'
-  get 'cars/new'
-  get 'cars/create'
-  get 'cars/update'
-  get 'cars/destroy'
+
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -16,7 +7,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :car, except: [:edit] do
-    resources :booking
+  resources :cars, except: [:edit] do
+    resources :bookings, only: %i[new update create pay]
   end
 end
