d065c668149d4b9961c027167d263d1243cbf5cd
Agustin Morere <amorere10@gmail.com>
2022-11-28 16:26:19 -0300

    Creacion de las rutas y los nombres de las acciones

---
+++ app/controllers/booking_controller.rb
@@ -0,0 +1,13 @@
+class BookingController < ApplicationController
+  def new
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def pay
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+  get 'booking/new'
+  get 'booking/update'
+  get 'booking/create'
+  get 'booking/pay'
+  get 'cars/index'
+  get 'cars/show'
+  get 'cars/new'
+  get 'cars/create'
+  get 'cars/update'
+  get 'cars/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :car, except: [:edit] do
+    resources :booking
+  end
 end
