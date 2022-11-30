c42c3ea64d99448cb4da15a0648b42969d32d192
Agustin Morere <amorere10@gmail.com>
2022-11-30 13:05:56 -0300

    Agregamos al controller del booking la creacion con usuario y auto

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,12 +1,16 @@
 class BookingController < ApplicationController
-  before_action :set_car, only: %i[show update destroy edit]
+  before_action :find_booking, only: %i[update edit]
 
   def new
+    @car = Car.find(params[:car_id])
     @booking = Booking.new
   end
 
   def create
+    @car = Car.find(params[:car_id])
     @booking = Booking.new(booking_params) #en que momento se le asigna el user_id y el car_id
+    @booking.user_id = current_user.id
+    @booking.car_id = @car.id
     if @booking.save
       redirect_to cars_path
     else
@@ -19,17 +23,17 @@ class BookingController < ApplicationController
   end
 
   def update
-    @car.update(booking_params)
-    redirect_to cars_path(@car)
-
-  end
-
-  def pay
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking) #esto es car_path? no existe el booking_path
   end
 
   private
 
   def booking_params
     params.require(:booking).permit(:check_in, :check_out, :pick_point, :drop_point, :time_check_in, :time_check_out )
+  end
+
+  def find_booking
+    @booking = Booking.find(params[:id])
   end
 end
