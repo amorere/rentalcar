289575552aa1bf8b72c10008aab2dedfd66e3314
julian <fattorinijota@gmail.com>
2022-12-02 15:53:17 -0300

    Termino los diseños de las cards, de booking y los formularios. Linkeado el boton de cancelar reserva.

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[new create]
   end
 
-  resources :bookings, only: %i[index edit update]
+  resources :bookings, only: %i[index edit update destroy]
 end
