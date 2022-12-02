f0cdfe92c701a890155a0ab1fa2864b1f9aa62e6
Gonzalo Raggio <graggiog@gmail.com>
2022-12-02 11:33:33 -0300

    Corrección seed, navbar, daily price en el show

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,5 +4,5 @@ class Car < ApplicationRecord
   has_many_attached :photos
 
   # validates that the car model is not empy
-  validates :model, :daily_price, :brand, :kms, :year, :plate, :color, :capacity, :load, :transmission, :technical_approval, :insurance, :performance, presence: true
+  validates :model, :daily_price, :year, :brand, :kms, :plate, :color, :capacity, :load, :transmission, :technical_approval, :insurance, :performance, presence: true
 end
