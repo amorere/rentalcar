babb8a10c8072c1051232785d8c7e9595034ca0a
Gonzalo Raggio <graggiog@gmail.com>
2022-12-02 11:38:31 -0300

    Navbar Solved

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,7 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many_attached :photos
-
+  # holi
   # validates that the car model is not empy
   validates :model, :daily_price, :year, :brand, :kms, :plate, :color, :capacity, :load, :transmission, :technical_approval, :insurance, :performance, presence: true
 end
